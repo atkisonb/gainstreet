10365e5426acf21c79e9ea62300b5b471e881bb1
Prelang Builder <builder@prelang.com>
2016-04-19 21:48:17 +0000

    Generated scaffolding for model 'BankAccount' via Rails

---
+++ app/views/bank_accounts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @bank_account, :id, :bank_account_number, :user_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :bank_accounts
+
   resources :users
 
   resources :funds
